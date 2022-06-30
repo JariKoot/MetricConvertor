b37ada22d6d85ecca995c5769054df8effdd49a2
JariKoot <82372836+JariKoot@users.noreply.github.com>
2022-06-30 17:29:03 +0200

    adding the dataset

---
+++ R/celcius_fahrenheit_data.R
@@ -0,0 +1,13 @@
+#' Celcius Fahrenheit translation table.
+#'
+#' Data describing new covid cases and hospital and ICU admissions across europe
+#'
+#' @format A data frame with 44 rows and 2 variables:
+#' \describe{
+#'   \item{Celsius}{degree Celcius}
+#'   \item{Fahrenheit}{degree Fahrenheit}
+#'   ...
+#' }
+#' @source \url{https://www.rapidtables.com/convert/temperature/celsius-to-fahrenheit-chart.html}
+
+"celcius_fahrenheit.Rdata"
